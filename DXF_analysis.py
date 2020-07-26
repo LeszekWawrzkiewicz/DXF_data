371b2bacffc47693160b1d5e6352e1968914b478
LW <lw@wp.pl>
2020-07-26 10:20:07 +0200

    update function neighbour frames

--- DXF_analysis.py
+++ DXF_analysis.py
@@ -2,6 +2,7 @@ import time
 import math
 import sqlite3 as lite
 
+
 class connection_insert_frame_X_elevation(dict):
     def __init__(self):
         self = dict()
@@ -14,6 +15,24 @@ start = time.time()
 path = r"C:\Users\Hp\Desktop\elewacja.dxf"
 import dxfgrabber
 from operator import itemgetter
+
+
+def distance(a, b, c, d):
+    return int(math.sqrt((a - c) ** 2 + (b - d) ** 2))
+
+
+def distance_checker(a, b):
+    if a['C_Y'] > b['Y2']:
+        result = int(distance(a['C_X'], a['C_Y'], b['C_X'], b['Y2']))
+    elif a['C_Y'] < b['Y1']:
+        result = int(distance(a['C_X'], a['C_Y'], b['C_X'], b['Y1']))
+    elif a['C_X'] > b['X2']:
+        result = int(distance(a['C_X'], a['C_Y'], b['X2'], b['C_Y']))
+    elif a['C_X'] < b['X1']:
+        result = int(distance(a['C_X'], a['C_Y'], b['X1'], b['C_Y']))
+    else:
+        result = 0
+    return result
 
 
 def module_size(temp_size):
@@ -55,14 +74,14 @@ for entity in dxf.entities:
             elevation_temp_data['X'] = int(entity.insert[0])
             elevation_temp_data['elev'] = entity.raw_text
             main_elevation_temp_data.append(elevation_temp_data)
-        if entity.raw_text[1] == "P":
+#        if entity.raw_text[1] == "P":
+        if "P" in entity.raw_text and len(entity.raw_text) in range(2,5):
             level_temp_data[int(entity.insert[1])] = entity.raw_text.replace("{", "").replace("}", "")
 # print(str(main_elevation_temp_data))
 temp_global_list = []
 temp_local_list = ()
 radius = ''
 # file_X_Y = open('temp_global_list.txt', 'w')
-
 '''calculation the location of the middle point of the frames by analysisi insert point of them,
 preparing the temporary data about the "radius", the radius is a half of hypotenuse the frame'''
 
@@ -78,67 +97,95 @@ for entity in dxf.entities:
             H_frame = list(filter(lambda H_data: H_data['code'] == entity.name, temp_code_W_H_total))[0][
                 'H']  # wysokość modułu
             temp_local_list = (entity.name, x_coordinate)
-            radius = math.sqrt((W_frame) ** 2 + (H_frame) ** 2)/2
+            radius = math.sqrt((W_frame) ** 2 + (H_frame) ** 2) / 2
             # file_X_Y.write(str(temp_local_list))
             dict_module_coordinate['C_X'] = int(
                 x_coordinate + W_frame / 2)  # C_X-calculation the middle point in horizontal direction - X-direction
             dict_module_coordinate['C_Y'] = int(
                 y_coordinate + H_frame / 2)  # C_Y-calculation the middle point in vertical direction - Y-direction
             dict_module_coordinate['radius'] = int(radius)
-            dict_module_coordinate['X1'] = dict_module_coordinate['C_X']-int(radius)
-            dict_module_coordinate['X2'] = dict_module_coordinate['C_X']+int(radius)
-            dict_module_coordinate['Y1'] = dict_module_coordinate['C_Y']-int(radius)
-            dict_module_coordinate['Y2'] = dict_module_coordinate['C_Y']+int(radius)
+            dict_module_coordinate['X1'] = dict_module_coordinate['C_X'] - int(radius)
+            dict_module_coordinate['X2'] = dict_module_coordinate['C_X'] + int(radius)
+            dict_module_coordinate['Y1'] = dict_module_coordinate['C_Y'] - int(radius)
+            dict_module_coordinate['Y2'] = dict_module_coordinate['C_Y'] + int(radius)
 
-            dict_module_coordinate['elev'] = ''
+            dict_module_coordinate['elev'] = 0
             dict_module_coordinate['level'] = ''
+
             temp_global_list.append(dict_module_coordinate)
             temp_local_list = ()
 
-# file_X_Y.close()
-
 sorted_list = sorted(temp_global_list, key=itemgetter("C_X", "C_Y"))
+#for i in sorted_list:
+#    print(i)
+
+
+sorted_list[0]['elev'] = 1
+#------------------------------------------
+trainer = []
+earlier_frame = sorted_list[0]
+elevation = 1
+
+for i in sorted_list:
+    current_trainer = i
+    if not trainer:
+        trainer.append(i)
+    else:
+        for j in trainer:
+            temp_result = distance_checker(i, j)
+            if i['elev'] == 0 and i['radius'] > temp_result:
+                i['elev'] = elevation
+                trainer.append(i)
+                break
+    temp_result = distance_checker(i, earlier_frame)
+    earlier_frame = i
+    if i['elev'] == 0 and i['radius'] < temp_result:
+        elevation = elevation + 1
+        i['elev'] = elevation
+        trainer.clear()
+        trainer.append(i)
+# ----------------------------------------
 file_X_Y = open('temp_global_list_total.txt', 'w')
 for i in sorted_list:
     file_X_Y.write(f"{str(i)}\n")
-    print(i)
+#    print(i)
 file_X_Y.close()
 
-line_list_X = []
+# line_list_X = []
 '''get the unique value of insert point to X-direction'''
-for i in sorted_list:
-    line_list_X.append(i['C_X'])
+# for i in sorted_list:
+#    line_list_X.append(i['C_X'])
 '''sorted list of the unique value of insert X-point on horizontal axis'''
-sorted_line_list_X = sorted(set(line_list_X))
+# sorted_line_list_X = sorted(set(line_list_X))
 '''merging unique insert X-point and the radius'''
-total_line_list_X_R = []
+# total_line_list_X_R = []
 '''find the "radius" value for the X-unique value'''
-for i in sorted_line_list_X:
-    line_list_X_R = {}
-    line_list_X_R['X'] = i
-    response = next((sub for sub in sorted_list if sub['C_X'] == i), None)
-    line_list_X_R['R'] = response['radius']
-    total_line_list_X_R.append(line_list_X_R)
-counter = 1
+# for i in sorted_line_list_X:
+#    line_list_X_R = {}
+#    line_list_X_R['X'] = i
+#    response = next((sub for sub in sorted_list if sub['C_X'] == i), None)
+#    line_list_X_R['R'] = response['radius']
+#    total_line_list_X_R.append(line_list_X_R)
+# counter = 1
 '''create dict with data of location the elevation' text'''
-X_elev_dict_obj = connection_insert_frame_X_elevation()
+# X_elev_dict_obj = connection_insert_frame_X_elevation()
 '''find the borders between the elevations'''
-previous_X = total_line_list_X_R[0]['X']
-previous_R = total_line_list_X_R[0]['R']
-X_elev_dict_obj.add(previous_X, counter)
-for i in total_line_list_X_R[1:]:
-    current_X = i['X']
-    current_R = i['R']
-    temp_connection_insert_frame_X_elevation = {}
-    if current_X - previous_X < current_R / 2 + previous_R / 2:
-        X_elev_dict_obj.add(current_X, counter)
-        previous_X = i['X']
-        previous_R = i['R']
-    else:
-        counter += 1
-        previous_X = i['X']
-        previous_R = i['R']
-        X_elev_dict_obj.add(previous_X, counter)
+# previous_X = total_line_list_X_R[0]['X']
+# previous_R = total_line_list_X_R[0]['R']
+# X_elev_dict_obj.add(previous_X, counter)
+# for i in total_line_list_X_R[1:]:
+#    current_X = i['X']
+#    current_R = i['R']
+#    temp_connection_insert_frame_X_elevation = {}
+#    if current_X - previous_X < current_R / 2 + previous_R / 2:
+#        X_elev_dict_obj.add(current_X, counter)
+#        previous_X = i['X']
+#        previous_R = i['R']
+#    else:
+#        counter += 1
+#        previous_X = i['X']
+#        previous_R = i['R']
+#        X_elev_dict_obj.add(previous_X, counter)
 
 frame_elevation_level = []
 '''sorting location of the "elevation" text'''
@@ -146,14 +193,15 @@ sorted_main_elevation_temp_data = sorted(main_elevation_temp_data, key=itemgette
 '''create a list of data: frame, elevation, level'''
 for i in temp_global_list:
     temp_frame_elevation_level = []
-    i['elev'] = X_elev_dict_obj[i['C_X']]
     Y_temp = int(i['C_Y'])
     temp_data = []
     temp_data = {abs(k - Y_temp): v for k, v in level_temp_data.items()}
     temp_lambda = lambda temp_data: min(temp_data.items())
-    i['level'] = temp_lambda(temp_data)[1].replace("{", "").replace("}", "")
+#    i['level'] = temp_lambda(temp_data)[1][:]
+    i['level'] = temp_lambda(temp_data)[1]
     temp_frame_elevation_level.append(i['name'])
-    temp_frame_elevation_level.append(sorted_main_elevation_temp_data[i['elev'] - 1]['elev'].replace("{", "").replace("}", ""))
+    temp_frame_elevation_level.append(sorted_main_elevation_temp_data[i['elev'] - 1]['elev'])
+#        sorted_main_elevation_temp_data[i['elev'] - 1]['elev'][1:-1])
     temp_frame_elevation_level.append(i['level'])
     frame_elevation_level.append(temp_frame_elevation_level)
 
@@ -168,8 +216,8 @@ with con:
     cur = con.cursor()
     cur.execute("SELECT * FROM frame_elevation_level")
     rows = cur.fetchall()
-#    for row in rows:
- #       print(row)
+    for row in rows:
+        print(row)
 
 end = time.time()
 print(end - start)
